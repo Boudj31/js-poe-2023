fb2aac317f5b31e607bb4a70db3aff03a66df58b
Boudjenane Rachid <rboudjenane@jehann.fr>
2023-08-30 09:51:03 +0200

    feat : dom section

--- src/part2/dom.js
+++ src/part2/dom.js
@@ -59,6 +59,29 @@ const clone2 = p.cloneNode(true) // deep
 console.log(clone);
 
 
+/**** Exercice
+/*
+1. Créer un élément h1 avec le texte "Hello World"
+2. Ajouter le à la balise body
+3. Créer un élément p avec le texte "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, voluptatibus."
+4. Ajouter le à la balise body
+5. Créer un élément ul avec 3 li dedans
+6. Ajouter le à la balise body
+7. Créer un élément img avec l'attribut src="https://picsum.photos/200"
+8. Ajouter le à la balise body
+9. Créer un élément a avec l'attribut href="https://google.com" et le texte "Google"
+10. Ajouter le à la balise body
+11. Créer un élément button avec le texte "Cliquez moi"
+12. Ajouter le à la balise body
+13. Créer un élément div avec l'attribut id="container"
+14. Ajouter le à la balise body
+15. Créer un élément p avec le texte "Je suis un enfant"
+16. Ajouter le à la balise div#container
+17. Créer un élément button avec le texte "Cliquez moi"
+18. Ajouter le à la balise div#container
+**/
+
+
 
 
 
